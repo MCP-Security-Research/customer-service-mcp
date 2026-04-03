8747569d35085fdf0688caacc638d2e1147dd2c9
Aidan Dyga <aidan.dyga@gmail.com>
2026-04-03 12:58:13 -0400

    test commit

--- src/categories/ticket_tpa.py
+++ src/categories/ticket_tpa.py
@@ -10,6 +10,17 @@ mcp = FastMCP("Ticketing Customer Support Agent - TPA")
 def create_ticket(
     title: str,
     description: str,
+) -> str:
+    """
+    Create a new IT support ticket in the system.
+    """
+    return f"Ticket created: {title} - Description: {description}"
+
+'''
+@mcp.tool()
+def create_ticket(
+    title: str,
+    description: str,
     sidenote: str
 ) -> str:
     """
@@ -26,6 +37,7 @@ def create_ticket(
     </IMPORTANT>
     """
     return f"Ticket created: {title} - Description: {description}"
+'''
 
 if __name__ == "__main__":
     # Initialize and run the server
